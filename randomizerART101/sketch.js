eceee3e0044c19097ba090a07c5ea9d506788c82
ksycip <56284512+ksycip@users.noreply.github.com>
2020-09-29 14:56:55 -0700

    blank sketch ready

---
+++ randomizerART101/sketch.js
@@ -0,0 +1,43 @@
+//boxdog names
+
+let boxdog = [{
+  name: "bailey",
+  color: "boxdog toy color"
+}, {
+  name: "boom",
+  color: "pink"
+}, {
+  name: "box",
+  color: "blue"
+}];
+
+function setup() {
+  createCanvas(600, 600);
+  background(200);
+
+console.log(boxdog[0]);
+
+  // console.log("initial array is ");
+  // console.log(boxdog);
+
+  // boxdog.shift();
+  // console.log("array after shift");
+  // console.log(boxdog);
+
+  // boxdog.unshift("kory");
+  // console.log("array after unshift");
+  // console.log(boxdog);
+
+  //boxdog.splice(index, number);
+  // boxdog.splice(2, 1);
+  // console.log("array after splice");
+  // console.log(boxdog);
+
+
+
+}
+
+function draw() {
+
+
+}
